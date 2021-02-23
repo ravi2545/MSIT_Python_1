5f5944f0c16432864c268f1fe9623e2c0d686046
ravi2545 <53468347+ravi2545@users.noreply.github.com>
2021-02-23 18:29:09 +0530

    Delete MSIT_Assignment directory

--- MSIT_Assignment/Assignment_set3/class_cricket.py
+++
@@ -1,9 +0,0 @@
-import re
-class Cricket:
-    def __init__(self, filenames):
-        self.filenames = filenames
-
-
-
-filenames = ["dravid.txt","sachin.txt","virat.txt","youvraj.txt"]
-ob = Cricket(filenames)
